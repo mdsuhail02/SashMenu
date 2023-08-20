c1a6e20f5a8ea250e2b63ed7bfc8ff73dedfb62f
nineleaps <mohamed.shoaib808@gmail.com>
2023-08-20 19:44:14 +0530

    Shoaib | Adds router to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ImageViewerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
